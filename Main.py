2229ab69a1fce66944c4487c73be9a81b85ac39a
Dashtiss <brandontisserand@outlook.com>
2023-12-31 22:30:32 -0600

    Fixed the Updater and bot

--- Main.py
+++ Main.py
@@ -1,21 +1,32 @@
 import requests
-import Bot
+import os
 
 def Update():
-    url = f'https://raw.githubusercontent.com/Dashtiss/Lightning-MC-Bot/master/main.py?token=GHSAT0AAAAAACIXF3ZXB6MRDA7MD4I57I3UZMSC5PA'
+    url = f'https://raw.githubusercontent.com/Dashtiss/Lightning-MC-Bot/master/Bot.py'
     response = requests.get(url)
 
     if response.status_code == 200:
         return response.text
     else:
-         print(f"Failed to retrieve file. Status code: {response.status_code}")
-         return None
+        print(f"Failed to retrieve file. Status code: {response.status_code}")
+        return None
 
 
 if __name__ == "__main__":
-    Update = Update()
-    if Update() is None:
-        raise "File is blank"
-
-
-
+    update = Update()
+    if update is None:
+        raise "File is Error"
+    if os.path.exists("Bot.py"):
+        with open("Bot.py", "r", encoding="utf-8") as BotFile:
+            bot = BotFile.read()
+        if bot != update:
+            print("Updating Bot")
+            with open("Bot.py", "w", encoding="utf-8") as BotFile:
+                BotFile.write(update)
+            import Bot
+            Bot.Run()
+    else:
+        with open("Bot.py", "w", encoding="utf-8") as BotFile:
+            BotFile.write(update)
+            import Bot
+            Bot.Run()
