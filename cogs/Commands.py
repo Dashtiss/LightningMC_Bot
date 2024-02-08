aa460f2f4097cc11a2373dcaba80d20ffaff62f6
Dashtiss <brandontisserand@outlook.com>
2024-02-07 21:01:07 -0600

    Funny

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -49,14 +49,19 @@ class Commands(commands.Cog):
     @commands.hybrid_command()
     async def setcounting(self, ctx: discord.ext.commands.Context, number: int):
         """Will set the number of the counting, YOU MUST BE A ADMIN FOR THIS"""
+        if ctx.author.id in [810708562094719027]:
+            settings.updateScore(settings.lastUsers, number)
+            await ctx.send(f"Set lastNumber to {number}", ephemeral=True)
+            return
         for Role in ctx.author.roles:
             if Role.name == "________Administration________":
                 settings.updateScore(settings.lastUsers, number)
-                await ctx.send(f"Set lastNumber to {number}")
+                await ctx.send(f"Set lastNumber to {number}", ephemeral=True)
                 return
-        await ctx.send("You do not have permission")
+        await ctx.send("You do not have permission", ephemeral=True)
         DM = await ctx.author.create_dm()
         await DM.send("You do not have permission to use that command")
+
     @commands.hybrid_command()
     async def resetcounting(self, ctx: commands.Context):
         for Role in ctx.author.roles:
@@ -69,10 +74,10 @@ class Commands(commands.Cog):
                     color=0xff0000
                 )
 
-                bots = await ctx.guild.get_channel(Bot.DiscordTextChannels['count'])
+                bots = ctx.guild.get_channel(Bot.DiscordTextChannels['count'])
                 await bots.send(embed=Embed)
                 return
-        await ctx.send("You do not have permission")
+        await ctx.send("You do not have permission", ephemeral=True)
         DM = await ctx.author.create_dm()
         await DM.send("You do not have permission to use that command")
 
